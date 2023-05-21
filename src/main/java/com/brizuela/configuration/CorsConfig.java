83dd2fe5da157ac1a05121b91f72753b553a6260
Julio Cesar Brizuela <brizuelaalvarado@gmail.com>
2023-05-21 15:21:57 -0600

    Agregando documentacion swagger

---
+++ src/main/java/com/brizuela/configuration/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.brizuela.configuration;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry
+                        .addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+
+                registry
+                        .addMapping("/login")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
