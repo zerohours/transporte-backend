cfb1ede812d5b338d9b08a7bf81f7d7f1b36a1b3
Julio Cesar Brizuela <brizuelaalvarado@gmail.com>
2023-05-21 22:07:04 -0600

    Fix por campos no-validos

--- src/main/java/com/brizuela/model/EnvioTipoTerrestre.java
+++ src/main/java/com/brizuela/model/EnvioTipoTerrestre.java
@@ -15,11 +15,11 @@ import javax.validation.constraints.Pattern;
 public class EnvioTipoTerrestre extends Envio {
 
     @NotNull(message = "Bodega de entrega es requerido")
-    @Column(name = "bodega_entrega", nullable = false)
+    @Column(name = "bodega_entrega")
     private String bodegaEntrega;
 
     @NotNull(message = "Numero de placa es requerido")
-    @Column(name = "numero_placa", nullable = false)
+    @Column(name = "numero_placa")
     @Pattern(regexp = "[A-Za-z]{3}[0-9]{3}",message = "El numero de flota debe tener el formato ###$$$")
     private String numeroPlaca;
 
